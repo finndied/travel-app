90bd977cbc022e6977bc2127219c7859be16d4d7
Ilya Morozov <finnlivefx1@mail.ru>
2023-04-18 14:32:05 +0700

    installed router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage/HomePage'
+
 export default function App() {
-	return <></>
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route index path='/' element={<HomePage />} />
+				</Routes>
+			</Router>
+		</>
+	)
 }
